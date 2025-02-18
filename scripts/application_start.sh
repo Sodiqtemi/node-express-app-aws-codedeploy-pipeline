74b1a1b1b30e9906d90bc87247098a60a187f576
Vic <victordickson474@gmail.com>
2025-02-18 14:11:27 +0100

    added ecosystem.config.js

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,8 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
 
+echo 'cd /home/ec2-user/node-express-app-aws-codedeploy-pipeline' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
+cd /home/ec2-user/node-express-app-aws-codedeploy-pipeline >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
+
 echo 'pm2 restart ecosystem.config.js' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
 pm2 restart ecosystem.config.js >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log

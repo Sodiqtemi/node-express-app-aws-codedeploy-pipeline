d68c2a20135bbb381a50b3f4e2dc2c2f797755f4
Vic <victordickson474@gmail.com>
2025-02-25 11:24:20 +0100

    removed  ecosystem.config.js

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,5 +5,5 @@ echo 'run application_start.sh: ' >> /home/ec2-user/node-express-app-aws-codedep
 echo 'cd /home/ec2-user/node-express-app-aws-codedeploy-pipeline' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
 cd /home/ec2-user/node-express-app-aws-codedeploy-pipeline >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
 
-echo 'pm2 restart ecosystem.config.js' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
-pm2 restart ecosystem.config.js >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
+echo 'sudo systemctl restart pm2-ec2-user' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
+sudo systemctl restart pm2-ec2-user >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log

81d124eb69e43a59a7210d3251a7a95ed48dbd7d
Vic <victordickson474@gmail.com>
2025-02-25 11:50:02 +0100

    removed  ecosystem.config.js

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
 
-echo 'cd /home/ec2-user/node-express-app-aws-codedeploy-pipeline' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
-cd /home/ec2-user/node-express-app-aws-codedeploy-pipeline >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
+cd /home/ec2-user/node-express-app-aws-codedeploy-pipeline 
+cp /home/ec2-user/ecosystem.config.js .
 
-echo 'npm install' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log 
-npm install >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
+npm install .
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-echo 'run application_start.sh: ' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
-
-
-echo 'sudo systemctl restart pm2-ec2-user' >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
-sudo systemctl restart pm2-ec2-user >> /home/ec2-user/node-express-app-aws-codedeploy-pipeline/deploy.log
+ 
+sudo systemctl restart pm2-ec2-user 

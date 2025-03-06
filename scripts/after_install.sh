7f4875c47103f4c8397b10f3e722bbeb21249a64
Vic <victordickson474@gmail.com>
2025-03-06 22:50:49 +0100

    test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Get the env file here
-cd /home/ec2-user/node-express-app-aws-codedeploy-pipeline 
-#cp /home/ec2-user/env/.env .
+cd /home/ubuntu/node-express-app-aws-codedeploy-pipeline 
+#cp /home/ubuntu/env/.env .
 
 pwd
 ls -al
@@ -12,13 +12,13 @@ ls -al
 # yarn build
 
 npm install
-#pm2 start app.js
+pm2 start app.js
 
 #####
 pm2 save
 pm2 startup
 
 # if it's a typescript app, then you'll run build (npm run build)
-# systemd setup
-#sudo env PATH=$PATH:/home/ec2-user/.nvm/versions/node/v22.14.0/bin /home/ec2-user/.nvm/versions/node/v22.14.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
-#sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
+#systemd setup
+sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v22.14.0/bin /home/ubuntu/.nvm/versions/node/v22.14.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  
 # start the process
-sudo systemctl restart pm2-ec2-user
+sudo systemctl restart pm2-ubuntu
